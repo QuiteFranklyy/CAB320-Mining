3efaa6b848317d5ceefd70a48411642e4661212c
Sebastian Young <franklyfrank00@gmail.com>
2021-04-25 14:16:23 +1000

    deleted frederick

--- mining.py
+++ mining.py
@@ -319,8 +319,6 @@ class Mine(search.Problem):
             return '\n'.join('level {}\n'.format(z)
                              + str(self.underground[..., z]) for z in range(self.len_z))
 
-            return self.underground[loc[0], loc[1], :]
-
     @staticmethod
     def plot_state(state):
         if state.ndim == 1:
